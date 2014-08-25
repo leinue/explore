24ae0ac4ebbb71911a0853419528f76cc00eaae1
ivy <597055914@qq.com>
2014-08-25 16:11:09 +0800

    update hot area page

---
+++ includes/hotaeras.php
@@ -0,0 +1,94 @@
+
+<div class="hotarea-card">
+	<div class="hotarea-left">
+
+		<div "hotarea-card-main">
+
+			<div class="hotarea-card-main-heading">
+				<div class="hotarea-card-main-heading-photo">
+					<img src="user/ivydom/photo.jpg" alt="ivydom" class="img-thumbnail" width="46" height="46">
+				</div>
+				<div class="hotarea-card-main-heading-editor">
+					<ul>
+						<a href=""><li>ivydom</li></a>
+						<li><a href="">公开分享</a>  -  上午11:06</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="hotarea-card-main-content">
+				<div class="hotarea-card-text">
+					Over-under photography<br>半水面写真<br>Bora Bora, French Polynesia﻿
+				</div>
+				<div class="hotarea-card-img">
+					<a href=""><img src="user/ivydom/background.jpg" alt="ivydom" width="330" height="265"></a>
+				</div>
+			</div>
+
+			<div class="hotarea-card-main-footer">
+				<div class="btn-group">
+  					<button type="button" class="btn btn-default">赞</button>
+  					<button type="button" class="btn btn-default">踩</button>
+ 					<button type="button" class="btn btn-default">收藏</button>
+				</div>
+				<div class="hotarea-comment">
+					<textarea class="form-control" id="twitter-content" rows="1" required></textarea>
+					<div class="people-lib-send-msg">
+						<ul class="people-lib-send-msg-menu">
+							<li title="#话题"><span class="glyphicon glyphicon-comment"></span></li>
+							<li title="@某人"><span class="glyphicon glyphicon-volume-up"></span></li>
+						</ul>
+						<div class="people-lib-send-msg-button"><button type="button" id="btn-twitter-send" class="btn btn-success btn-sm">发布</button></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="hotarea-right">
+
+		<div "hotarea-card-main">
+
+			<div class="hotarea-card-main-heading">
+				<div class="hotarea-card-main-heading-photo">
+					<img src="user/ivydom/photo.jpg" alt="ivydom" class="img-thumbnail" width="46" height="46">
+				</div>
+				<div class="hotarea-card-main-heading-editor">
+					<ul>
+						<a href=""><li>ivydom</li></a>
+						<li><a href="">公开分享</a>  -  上午11:06</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="hotarea-card-main-content">
+				<div class="hotarea-card-text">
+					头等舱是否可以优先登机？而且你也说了，这是学军窗口。=军人抚恤优待条例第三十六条 现役军人凭有效证件、残疾军人凭《中华人民共和国残疾军人证》优先购票乘坐境内运行的火车、轮船、长途公共汽车以及民航班机；残疾军人享受减收正常票价50%的优待。
+				</div>
+				<div class="hotarea-card-img">
+					<a href=""><img src="http://pic3.zhimg.com/dd5ea09853ba716a43017237ad5565e0_r.jpg" alt="ivydom" width="330" height="265"></a>
+				</div>
+			</div>
+
+			<div class="hotarea-card-main-footer">
+				<div class="btn-group">
+  					<button type="button" class="btn btn-default">赞</button>
+  					<button type="button" class="btn btn-default">踩</button>
+ 					<button type="button" class="btn btn-default">收藏</button>
+				</div>
+				<div class="hotarea-comment">
+					<textarea class="form-control" id="twitter-content" rows="1" required></textarea>
+					<div class="people-lib-send-msg">
+						<ul class="people-lib-send-msg-menu">
+							<li title="#话题"><span class="glyphicon glyphicon-comment"></span></li>
+							<li title="@某人"><span class="glyphicon glyphicon-volume-up"></span></li>
+						</ul>
+						<div class="people-lib-send-msg-button"><button type="button" id="btn-twitter-send" class="btn btn-success btn-sm">发布</button></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
