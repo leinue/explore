9bf3205c71e11f94085646eff2ca0f79629bfbff
ivy <597055914@qq.com>
2014-08-25 10:42:10 +0800

    update profile card of setting page

--- includes/setting.php
+++ includes/setting.php
@@ -1,6 +1,7 @@
 
 <div class="user-card">
 	<div class="user-setting-left">
+
 		<div class="setting-user-card">
 			<div class="setting-change-ptoto">
 				<div class="setting-face">
@@ -15,17 +16,49 @@
 					<span class="setting-face-background-title">背景</span>					
 					<img src="user/ivydom/background.jpg" alt="ivydom" width="70" height="70" class="img-rounded">
   					<div class="btn-group">
-    					<button type="button" class="btn btn-default">更换头像</button>
+    					<button type="button" class="btn btn-default">更换背景</button>
   					</div>
-  					<span class="setting-help-block">推荐1500*500,支持.jpg|.png|.bmp</span>		
+  					<span class="setting-help-block">支持.jpg|.png|.bmp</span>		
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="user-setting-right">
+
 		<div class="setting-user-card">
 		fdfdgfffd
 		</div>
+
+	</div>
+	<div class="user-setting-right">
+
+		<div class="setting-user-card">
+			<div class="setting-profile">
+				<div class="lib-card-heading">基本资料</div>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="全名">
+    			</div>
+    			<span class="help-block">随便取</span>
+   				<div class="input-group">
+					<input type="text" class="form-control" placeholder="地点">
+    			</div>
+    			<span class="help-block">比如"中二之峰学院"</span>
+    			<div class="input-group">
+					<input type="text" class="form-control" placeholder="性别">
+    			</div>
+    			<span class="help-block">人妖,变性人,futa,我们都可以接受</span>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="一句话名片">
+    			</div>
+    			<span class="help-block">比如,专注拾荒三十年,限15个字</span>
+				<textarea class="form-control" id="twitter-content" rows="2" onkeyup="alterBtnStatus()" required></textarea>
+    			<span class="help-block">你去过多少地方?用英文逗号分割.</span>
+				<textarea class="form-control" id="twitter-content" rows="2" onkeyup="alterBtnStatus()" required></textarea>
+				<span class="help-block">160个字的个人介绍</span>
+				<div class="setting-card-footer">
+    				<button type="button" class="btn btn-default">保存更改</button>
+				</div>
+			</div>
+		</div>
+
 	</div>
 	
 </div>
