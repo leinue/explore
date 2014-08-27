803d182299f2a411ce6b77b69bc21a42f8d9acd0
ivy <597055914@qq.com>
2014-08-27 13:54:00 +0800

    update the function of writing dynamic

--- functions/fun.php
+++ functions/fun.php
@@ -16,46 +16,46 @@ function getIP(){
 class accessExplore extends mysqlManager{
 
 	public $regResult;
+	public $pdo;
 
-	function __construct($email,$name,$password){
-
-	}
+	function __construct(PDO $_pdo){$this->pdo=$_pdo;}
 
 	function regExplore($email,$name,$password){
 
 		date_default_timezone_set("Etc/GMT+8");
 
-		parent::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //禁用prepared statements的仿真效果
+		$this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //禁用prepared statements的仿真效果
 
 		$userFolder="user/default/";
 		$nowTime=date('Y-m-d H:i:s',time());
-
-		$objToolKit=new toolKit;
-		$userIp=$objToolKit->getIP();
+		$userIp=getIP();
 
 		$sql="INSERT INTO `basicprofile`(`email`, `name`, `password`, `location`, `sex`, `intro`, `detailIntro`, `face`, `background`,`backgroundBlur`, `emailVerified`, `place`, `nowPlace`, `ip`, `regTime`, `lastLoginTime`) 
-		VALUES (?,?,SHA1(?),'China','futa','hh','23333',?,?,?,0,'China','China',?,?,?)";
-			
-		$stmt=parent::$pdo->prepare($sql);
+		VALUES (?,?,SHA1(?),'China','futa','explore','explore',?,?,?,0,'China','China',?,?,?)";
+
+		$stmt=$this->pdo->prepare($sql);
 
 		if($stmt!=false){
+
 			$exeres=$stmt->execute(array($email, $name,$password,$userFolder."photo.jpg",$userFolder."background.jpg",$userFolder."backgroundBlur.jpg",$userIp,$nowTime,$nowTime)); 
 
 			if($exeres){
-				$this->regResult=true;
+				$sql_dynamic="INSERT INTO `dynamic`(`uid`, `dynamic`) VALUES (".$this->pdo->lastInsertId().",'注册了Explore')";
+				$rowAffectedNum=$this->pdo->exec($sql_dynamic);
+
+				if ($rowAffectedNum!=0) {
+					$this->regResult=true;
+				}else{$this->regResult=false;}
+
 			}else{
 				print_r($stmt->errorInfo());
 				$this->regResult=false;
 			}
+
 		}else{$this->regResult=false;}
 	}
 
 }
-
-//$cm=new accessExplore('explore','localhost','root','xieyang');
-//$cm->connectMysql();
-
-//$re=new regExplore('597055914','ivy','77585219966');
 
 $dbname="explore";
 $host="localhost";
@@ -67,5 +67,8 @@ try {
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
+
+$accEx=new accessExplore($pdo);
+$accEx->regExplore('597055914@qq.com','ivy','7758521x');
 
 ?>
