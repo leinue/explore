34d45a12cd4c2843c40d478308594c2470540399
ivy <597055914@qq.com>
2014-08-27 10:28:27 +0800

    fix register issue

--- functions/fun.php
+++ functions/fun.php
@@ -1,12 +1,25 @@
 <?php
 require('mysql.php');
 
-$cm=mysqlManager::getPDO();
-$cm->connectMysql('explore','localhost','root','xieyang');
+class toolKit{
 
-//$cm::$pdo->exec();
+	function getIP(){
+		global $ip;
+		if (getenv("HTTP_CLIENT_IP"))
+			$ip = getenv("HTTP_CLIENT_IP");
+		else if(getenv("HTTP_X_FORWARDED_FOR"))
+			$ip = getenv("HTTP_X_FORWARDED_FOR");
+		else if(getenv("REMOTE_ADDR"))
+			$ip = getenv("REMOTE_ADDR");
+		else $ip = "Unknow";
+		return $ip;
+	}
+	
+}
 
 class regExplore extends mysqlManager{
+
+	public $regResult;
 
 	function __construct($email,$name,$password){
 
@@ -15,23 +28,36 @@ class regExplore extends mysqlManager{
 		parent::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //禁用prepared statements的仿真效果
 
 		$userFolder="user/default/";
-		$nowTime=time('Y-m-d H:m:s');
-		$userIp=$_SERVER["REMOTE_ADDR"];
+		$nowTime=date('Y-m-d H:i:s',time());
 
-		$sql="INSERT INTO `basicprofile`(`email`, `name`, `password`, `location`, `sex`, `intro`, `detailIntro`, `face`, `background`,`backgroundBlur`, `emailVerified`, `place`, `nowPlace`, `ip`, `regTime`, `lastLoginTime`) VALUES (?,?,SHA1(?),China,futa,hh,23333,?,?,?,0,China,China,?,?,?)";
+		$objToolKit=new toolKit;
+		$userIp=$objToolKit->getIP();
 
+		$sql="INSERT INTO `basicprofile`(`email`, `name`, `password`, `location`, `sex`, `intro`, `detailIntro`, `face`, `background`,`backgroundBlur`, `emailVerified`, `place`, `nowPlace`, `ip`, `regTime`, `lastLoginTime`) 
+		VALUES (?,?,SHA1(?),'China','futa','hh','23333',?,?,?,0,'China','China',?,?,?)";
+			
 		$stmt=parent::$pdo->prepare($sql);
-		$exeres=$stmt->execute(array($email, $name,$password,$userFolder."/photo.jpg",$userFolder."/background.jpg",$userFolder."/backgroundBlur.jpg",$userIp,$nowTime,$nowTime)); 
 
-		if($exeres){
-			echo '注册成功';
-		}else{
-			print_r($stmt->errorInfo()); 
-		}
+		if($stmt!=false){
+			$exeres=$stmt->execute(array($email, $name,$password,$userFolder."photo.jpg",$userFolder."background.jpg",$userFolder."backgroundBlur.jpg",$userIp,$nowTime,$nowTime)); 
+
+			if($exeres){
+				$this->regResult=true;
+			}else{
+				print_r($stmt->errorInfo());
+				$this->regResult=false;
+			}
+		}else{$this->regResult=false;}
+		
 	}
 
 }
 
-$re=new regExplore('5dssd@sdsd.com','ieeee','reuttiysaa');
+$cm=mysqlManager::getPDO();
+$cm->connectMysql('explore','localhost','root','xieyang');
+
+//$cm::$pdo
+
+//$re=new regExplore('597055914','ivy','77585219966');
 
 ?>
